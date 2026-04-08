f20a1d59b7d656c83cd94ad74a0fc17883297b1a
Jaken Herman <JakenHerman7@Gmail.com>
2026-04-08 00:07:50 -0500

    Add 10 leds (all hardcoded to on for now)

--- astros-gameviewer.ino
+++ astros-gameviewer.ino
@@ -1,11 +1,25 @@
 #include "src/display.h"
 
+const int LED_PINS[] = {40, 41, 42, 43, 44, 45, 46, 47, 48, 49};
+const int LED_COUNT = 10;
+
 void setup() {
   Serial.begin(9600);
+  for (int i = 0; i < LED_COUNT; i++) {
+    pinMode(LED_PINS[i], OUTPUT);
+    digitalWrite(LED_PINS[i], HIGH);
+  }
   displayInit();
   displayUpdate("Astros Live", "HOU 7 - TEX 4");
 }
 
 void loop() {
-  // Main game viewer loop
+  // Software PWM ~30% brightness
+  const unsigned long period = 10000;
+  const unsigned long duty   =  3000;
+  unsigned long t = micros() % period;
+  bool on = (t < duty);
+  for (int i = 0; i < LED_COUNT; i++) {
+    digitalWrite(LED_PINS[i], on ? HIGH : LOW);
+  }
 }
